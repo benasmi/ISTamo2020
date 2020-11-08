9a1af5e83f0e29c1a198a1ec7eeed4693c7bfaad
Benas <miliunas@Benass-MacBook-Pro.local>
2020-11-08 14:06:15 +0200

    News page

--- src/pages/NewsPage1.js
+++ src/pages/NewsPage1.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import NewsCard from "../components/NewsCard";
+import Typography from "@material-ui/core/Typography";
 
 
 export default function NewsPage1(){
     return (
         <div>
-            News page
+            <div>
+                <Typography variant={'h3'}>
+                    Naujienos
+                </Typography>
+            </div>
+
+            <div style={{marginTop: 32}}>
+                <NewsCard onLike={()=>{
+                    alert("Vartotojas paspaudė patinka")
+                }} />
+            </div>
         </div>
     )
 }
